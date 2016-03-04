b31afdc188e301c3ef085871e9613f814db6e15a
Alexander Wagner <alexander.wagner@inside-m2m.de>
2016-03-04 14:00:40 +0100

    added hello world endpoint

--- src/main/java/net/qpid/monte/rest/RestApplication.java
+++ src/main/java/net/qpid/monte/rest/RestApplication.java
@@ -1,9 +1,20 @@
 package net.qpid.monte.rest;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+
+import net.qpid.monte.bot.helloworld.HelloWorldEndpoint;
 
 @ApplicationPath("/rest/api")
 public class RestApplication extends Application {
 
+	@Override
+	public Set<Class<?>> getClasses() {
+		Set<Class<?>> classes = new HashSet<>();
+		classes.add(HelloWorldEndpoint.class);
+		return super.getClasses();
+	}
 }
